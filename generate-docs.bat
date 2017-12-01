64d970c86609f2495513fd5d77e43d814edf8423
Kent Johnson <kent@fireshark.com>
2017-12-01 10:09:05 -0600

    Docs changes

--- generate-docs.bat
+++ generate-docs.bat
@@ -1,1 +1,18 @@
 XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Channel\bin\Debug\Butterfly.Channel.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Channel.EmbedIO\bin\Debug\Butterfly.Channel.EmbedIO.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Channel.RedHttpServer\bin\Debug\Butterfly.Channel.RedHttpServer.dll docs --visibility public --clean
+
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Client.DotNet\bin\Debug\Butterfly.Client.DotNet.dll docs --visibility public --clean
+
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Database\bin\Debug\Butterfly.Database.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Database.Event\bin\Debug\Butterfly.Database.Event.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Database.Memory\bin\Debug\Butterfly.Database.Memory.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Database.MySql\bin\Debug\Butterfly.Database.MySql.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Database.Postgres\bin\Debug\Butterfly.Database.Postgres.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Database.SQLite\bin\Debug\Butterfly.Database.SQLite.dll docs --visibility public --clean
+
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Util\bin\Debug\Butterfly.Util.dll docs --visibility public --clean
+
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Web\bin\Debug\Butterfly.Web.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Web.EmbedIO\bin\Debug\Butterfly.Web.EmbedIO.dll docs --visibility public --clean
+XmlDocMarkdown\tools\XmlDocMarkdown.exe Butterfly.Web.RedHttpServer\bin\Debug\Butterfly.Web.RedHttpServer.dll docs --visibility public --clean
