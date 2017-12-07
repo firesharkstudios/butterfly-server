9f0b70b1277e5dc82c0fad92c60791992c9af1d5
Kent Johnson <kent@fireshark.com>
2017-12-07 14:32:31 -0600

    Create nuspec files

--- Butterfly.Channel/Properties/AssemblyInfo.cs
+++ Butterfly.Channel/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Butterfly.Channel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allows clients to create new channels to the server and allows the server to push messages to connected clients (think WebSockets)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Butterfly.Channel")]
+[assembly: AssemblyCompany("Fireshark Studios, LLC")]
+[assembly: AssemblyProduct("Butterfly Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- Butterfly.Database/Properties/AssemblyInfo.cs
+++ Butterfly.Database/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Butterfly.Database")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allows executing SELECT statements, creating transactions to execute INSERT, UPDATE, and DELETE statements; creating dynamic views; and receiving data change events both on tables and dynamic views")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Butterfly.Database")]
+[assembly: AssemblyCompany("Fireshark Studios, LLC")]
+[assembly: AssemblyProduct("Butterfly Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
