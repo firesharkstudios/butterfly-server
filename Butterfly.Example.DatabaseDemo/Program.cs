53fefb7c6415b14c4b1419f49170296d900aa5c3
Kent Johnson <kent@fireshark.com>
2018-08-24 14:52:17 -0500

    Changes

--- Butterfly.Example.DatabaseDemo/Program.cs
+++ Butterfly.Example.DatabaseDemo/Program.cs
@@ -4,120 +4,139 @@
 using System;
 using System.Threading.Tasks;
 
-using NLog;
-
 using Butterfly.Core.Database;
+using Butterfly.Core.Database.Dynamic;
 using Butterfly.Core.Util;
 
-namespace Butterfly.Example.DatabaseDemo {
-    /*
-     * The database layer is perhaps the most interesting part of Butterfly Server .NET.
-     * 
-     * This simple demo...
-     * 
-     * 1) Creates a Memory database
-     * 2) Populates the database with sample data
-     * 3) Creates a DynamicQuery
-     * 4) Shows how various changes to the database impact the DynamicQuery
-     */
+namespace MyDatabaseTest {
     class Program {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
-
+        /*
+         * Requires creating a MySQL database like...
+         *   CREATE DATABASE butterfly;
+         *   USE butterfly;
+         *   CREATE TABLE user (
+         *     id VARCHAR(50) NOT NULL,
+         *     name VARCHAR(40) NOT NULL,
+         *     PRIMARY KEY(id)
+         *   );
+         *   CREATE TABLE todo (
+         *     id VARCHAR(50) NOT NULL,
+         *     name VARCHAR(40) NOT NULL,
+         *     user_id VARCHAR(50) NOT NULL,
+         *     is_done VARCHAR(1) NOT NULL,
+         *     PRIMARY KEY(id),
+         *     KEY user_id (user_id)
+         *   );
+         *   GRANT ALL PRIVILEGES ON butterfly.* TO 'test'@'localhost' IDENTIFIED BY 'test!123';
+         */
         static void Main(string[] args) {
-            var database = new Butterfly.Core.Database.Memory.MemoryDatabase();
-            //var database = new Butterfly.MySql.MySqlDatabase("Server=127.0.0.1;Uid=test;Pwd=test!123;Database=butterfly_test");
+            Console.WriteLine("You must create a 'butterfly' database using the commands in the comments and provide the appropriate connect string to run this demo");
 
-            var task = Run(database);
+            // Get a reference to a MySQL database (fix the connect string for your environment)
+            var database = new Butterfly.MySql.MySqlDatabase("Server=127.0.0.1;Uid=test;Pwd=test!123;Database=butterfly");
+
+            // Create ids like u_fffa58f2-ccd6-433b-91f0-d708e7ecd205
+            database.SetDefaultValue("id", tableName => $"{tableName.Abbreviate()}_{Guid.NewGuid().ToString()}");
+
+            Run(database);
             Console.ReadLine();
         }
 
         static async Task Run(IDatabase database) {
-            database.CreateFromText(@"CREATE TABLE user_todo (
-	            id VARCHAR(50) NOT NULL,
-	            name VARCHAR(40) NOT NULL,
-                username VARCHAR(25) NOT NULL,
-	            PRIMARY KEY(id),
-                INDEX username (username)
-            );");
-
-            // Create ids like ut_fffa58f2-ccd6-433b-91f0-d708e7ecd205
-            database.SetDefaultValue("id", tableName => $"{tableName.Abbreviate()}_{Guid.NewGuid().ToString()}");
-
-            // First, let's add some sample records
-            string task1Id;
-            string task2Id;
-            string task3Id;
-            using (ITransaction transaction = await database.BeginTransactionAsync()) {
-                task1Id = await transaction.InsertAsync<string>("user_todo", new {
-                    name = "Task #1",
-                    username = "spongebob"
+            // Create sample data
+            string spongebobId, patrickId;
+            string todo1Id, todo2Id, todo3Id, todo4Id;
+            using (var transaction = await database.BeginTransactionAsync()) {
+                // Create Spongebob and Patrick user records
+                spongebobId = await transaction.InsertAsync<string>("user", new {
+                    name = "Spongebob",
                 });
 
-                task2Id = await transaction.InsertAsync<string>("user_todo", new {
-                    name = "Task #2",
-                    username = "spongebob"
+                patrickId = await transaction.InsertAsync<string>("user", new {
+                    name = "Patrick",
                 });
 
-                task3Id = await transaction.InsertAsync<string>("user_todo", new {
-                    name = "Task #3",
-                    username = "patrick"
+                // Create some todo records
+                todo1Id = await transaction.InsertAsync<string>("todo", new {
+                    name = "Todo #1",
+                    user_id = spongebobId,
+                    is_done = "N",
                 });
 
+                todo2Id = await transaction.InsertAsync<string>("todo", new {
+                    name = "Todo #2",
+                    user_id = spongebobId,
+                    is_done = "N",
+                });
+
+                todo3Id = await transaction.InsertAsync<string>("todo", new {
+                    name = "Todo #3",
+                    user_id = spongebobId,
+                    is_done = "Y",
+                });
+
+                todo4Id = await transaction.InsertAsync<string>("todo", new {
+                    name = "Todo #4",
+                    user_id = patrickId,
+                    is_done = "N",
+                });
+
+                // Don't forget to commit the changes
                 await transaction.CommitAsync();
             }
 
-            // Next, let's create a DynamicView that will echo the initial matching records
-            // to the Console and will also echo any changes to the records to the Console
-            logger.Info("Creating the DynamicView...");
-            var dynamicViewSet = database.CreateAndStartDynamicViewAsync(
-                "SELECT * FROM user_todo WHERE username=@username",
-                dataEventTransaction => {
-                    var json = JsonUtil.Serialize(dataEventTransaction, format: true);
-                    logger.Info(json);
+            // Create a DynamicViewSet that...
+            // - Uses a SELECT statement to define a resultset joining the todo and user tables filtered by isDone="N"
+            // - Echoes all data events to the Console
+            // Note that all the initial matching records are echoed to the Console as Initial events.
+            using (DynamicViewSet dynamicViewSet = await CreateDynamicViewSet(database, "N")) {
+
+                // This will echo an Insert data event to the Console because it matches the SELECT criteria
+                await database.InsertAndCommitAsync<string>("todo", new {
+                    name = "Task #5",
+                    user_id = spongebobId,
+                    is_done = "N",
+                });
+
+                // This will NOT echo an Insert data event to the Console because it does NOT match the SELECT criteria
+                await database.InsertAndCommitAsync<string>("todo", new {
+                    name = "Task #6",
+                    user_id = spongebobId,
+                    is_done = "Y",
+                });
+
+                // This will echo an Update data event to the Console because it matches the SELECT criteria
+                await database.UpdateAndCommitAsync("todo", new {
+                    id = todo1Id,
+                    name = "Updated Task #1"
+                });
+
+                // This will echo two Update data events to the Console because it impacts two records in the resultset
+                // Yes, that's pretty cool :)
+                await database.UpdateAndCommitAsync("user", new {
+                    id = spongebobId,
+                    name = "Mr. Spongebob"
+                });
+            }
+        }
+
+        static Task<DynamicViewSet> CreateDynamicViewSet(IDatabase database, string isDoneFilter) {
+            int seq = 0;
+            Console.WriteLine("Creating the DynamicView...");
+            return database.CreateAndStartDynamicViewAsync(
+                @"SELECT t.id, t.name todo_name, u.name user_name
+                FROM todo t 
+                    INNER JOIN user u ON t.user_id=u.id
+                WHERE is_done=@isDoneFilter",
+                dataEvents => {
+                    var json = JsonUtil.Serialize(dataEvents, format: true);
+                    seq++;
+                    Console.WriteLine($"#{seq}:dataEvents={json}");
                 },
                 new {
-                    username = "spongebob"
+                    isDoneFilter
                 }
             );
-
-            // Next, let's INSERT a record that matches our resultset criteria above
-            // (this will trigger the DynamicView handler above to write the change to the Console)
-            logger.Info("Inserting a matching record...");
-            await database.InsertAndCommitAsync<string>("user_todo", new {
-                name = "Task #4",
-                username = "spongebob"
-            });
-
-            // Next, let's INSERT a record that does NOT match our resultset criteria
-            // (this will NOT trigger the DynamicView handler above to write the change to the Console)
-            logger.Info("Inserting a non-matching record...");
-            await database.InsertAndCommitAsync<string>("user_todo", new {
-                name = "Task #5",
-                username = "patrick"
-            });
-
-            // Next, let's UPDATE a record that matches our resultset criteria above
-            // (this will trigger the DynamicView handler above to write the change to the Console)
-            logger.Info("Updating a matching record...");
-            await database.UpdateAndCommitAsync("user_todo", new {
-                id = task1Id,
-                name = "Updated Task #1"
-            });
-
-            // Next, let's UPDATE a record that does NOT match our resultset criteria above
-            // (this will NOT trigger the DynamicView handler above to write the change to the Console)
-            logger.Info("Updating a non-matching record...");
-            await database.UpdateAndCommitAsync("user_todo", new {
-                id = task3Id,
-                name = "Updated Task #3"
-            });
-
-            // Discard the DynamicView
-            dynamicViewSet.Dispose();
-
-            // This demo used a single table query; however, multiple table queries with 
-            // INNER and LEFT joins are also supported 
-            // (requires a database supporting joins like MySQL, Postgres, SQLite, etc) 
         }
     }
 }
