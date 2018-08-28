127c05a942baf4b819d5be41906711b205bc5872
Johan van Rensburg <jhnvnrnsbrg@gmail.com>
2018-08-28 22:47:24 +0200

    Add initial project

---
+++ Butterfly.SqlServer/SqlServerDatabase.cs
@@ -0,0 +1,43 @@
+﻿using Butterfly.Core.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butterfly.SqlServer
+{
+	public class SqlServerDatabase : BaseDatabase
+	{
+		public SqlServerDatabase(string connectionString) : base(connectionString) {
+		}
+
+		public override bool CanJoin => throw new NotImplementedException();
+
+		public override bool CanFieldAlias => throw new NotImplementedException();
+
+		protected override BaseTransaction CreateTransaction()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<Dictionary<string, object>[]> DoQueryRowsAsync(string storedProcedureName, Dictionary<string, object> executableParams)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<Dictionary<string, object>[]> DoSelectRowsAsync(string executableSql, Dictionary<string, object> executableParams)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task LoadSchemaAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<Table> LoadTableSchemaAsync(string tableName)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Butterfly.SqlServer/SqlServerTransaction.cs
@@ -0,0 +1,74 @@
+﻿using Butterfly.Core.Database;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Butterfly.SqlServer
+{
+	public class SqlServerTransaction : BaseTransaction
+	{
+		public SqlServerTransaction(SqlServerDatabase sqlServerDatabase) : base(sqlServerDatabase)
+		{
+		}
+
+		public override void Begin()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Task BeginAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void DoCommit()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task DoCommitAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override bool DoCreate(CreateStatement statement)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<bool> DoCreateAsync(CreateStatement statement)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<int> DoDeleteAsync(string executableSql, Dictionary<string, object> executableParams)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<Func<object>> DoInsertAsync(string executableSql, Dictionary<string, object> executableParams, bool ignoreIfDuplicate)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void DoRollback()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task DoTruncateAsync(string tableName)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task<int> DoUpdateAsync(string executableSql, Dictionary<string, object> executableParams)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
