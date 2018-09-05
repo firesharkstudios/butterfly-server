a86b27195cff7e066861101d9f7abf506a578b52
Kent Johnson <kent@fireshark.com>
2018-09-05 16:40:06 -0500

    Better docs

--- Butterfly.Core/WebApi/IWebApi.cs
+++ Butterfly.Core/WebApi/IWebApi.cs
@@ -7,36 +7,35 @@ using System.Threading.Tasks;
 
 namespace Butterfly.Core.WebApi {
     /// <summary>
-    /// Allows receiving API requests via HTTP (inspired by Node.js' Express)<para/>
+    /// Allows handling requests/responses to a Web API via HTTP (inspired by Node.js' Express)<para/>
     /// </summary>
-    /// <remarks>
-    /// Example usage...<para/>
+    /// <example>
     /// <code>
-    ///     webApi.OnGet("/chat-messages", async(req, res) => {
-    ///         // Handle the request and return any objects as needed (will be JSON encoded)
-    ///     });
-    ///     webApi.OnPost("/login", async(req, res) => {
-    ///         // Handle the request and return any objects as needed (will be JSON encoded)
-    ///     });
-    ///     webApi.Compile();
+    /// webApi.OnGet("/chat-messages", async(req, res) => {
+    /// // Handle the request and return any objects as needed (will be JSON encoded)
+    /// });
+    /// webApi.OnPost("/login", async(req, res) => {
+    /// // Handle the request and return any objects as needed (will be JSON encoded)
+    /// });
+    /// webApi.Compile();
     /// </code>
-    /// </remarks>
+    /// </example>
     public interface IWebApi : IDisposable {
         /// <summary>
         /// Adds a <paramref name="listener"/> executed when DELETE requests are received matching <paramref name="path"/>
         /// </summary>
-        /// <remarks>
+        /// <example>
         /// <code>
-        ///     webApi.OnDelete("/api/todo", async(req, res) => {
-        ///         var id = await req.ParseAsJsonAsync&lt;string&gt;();
-        ///         await database.DeleteAndCommitAsync("todo", id);
-        ///     });
-        ///     webApi.OnDelete("/api/todo/{id}", async(req, res) => {
-        ///         var id = req.PathParams.GetAs("id", "");
-        ///         await database.DeleteAndCommitAsync("todo", id);
-        ///     });
+        /// webApi.OnDelete("/api/todo", async(req, res) => {
+        /// var id = await req.ParseAsJsonAsync&lt;string&gt;();
+        /// await database.DeleteAndCommitAsync("todo", id);
+        /// });
+        /// webApi.OnDelete("/api/todo/{id}", async(req, res) => {
+        /// var id = req.PathParams.GetAs("id", "");
+        /// await database.DeleteAndCommitAsync("todo", id);
+        /// });
         /// </code>    
-        /// </remarks>
+        /// </example>
         /// <param name="path">Only execute the <paramref name="listener"/> when DELETE requests match this path.</param>
         /// <param name="listener">Execute this async handler when a DELETE request is received matching the <paramref name="path"/></param>
         void OnDelete(string path, Func<IHttpRequest, IHttpResponse, Task> listener);
@@ -44,19 +43,19 @@ namespace Butterfly.Core.WebApi {
         /// <summary>
         /// Adds a <paramref name="listener"/> executed when GET requests are received matching <paramref name="path"/>
         /// </summary>
-        /// <remarks>
+        /// <example>
         /// <code>
-        ///     webApi.OnGet("/api/todos", async(req, res) => {
-        ///         Dict[] rows = await database.SelectRowsAsync("SELECT * FROM todo");
-        ///         await res.WriteAsJsonAsync(rows);
-        ///     });
-        ///     webApi.OnGet("/api/todo/{id}", async(req, res) => {
-        ///         var id = req.PathParams.GetAs("id", "");
-        ///         Dict row = await database.SelectRowAsync("SELECT * FROM todo", id);
-        ///         await res.WriteAsJsonAsync(row);
-        ///     });
+        /// webApi.OnGet("/api/todos", async(req, res) => {
+        ///     Dict[] rows = await database.SelectRowsAsync("SELECT * FROM todo");
+        ///     await res.WriteAsJsonAsync(rows);
+        /// });
+        /// webApi.OnGet("/api/todo/{id}", async(req, res) => {
+        ///     var id = req.PathParams.GetAs("id", "");
+        ///     Dict row = await database.SelectRowAsync("SELECT * FROM todo", id);
+        ///     await res.WriteAsJsonAsync(row);
+        /// });
         /// </code>
-        /// </remarks>
+        /// </example>
         /// <param name="path">Only execute the <paramref name="listener"/> when GET requests match this path.</param>
         /// <param name="listener">Execute this async handler when a GET request is received matching the <paramref name="path"/></param>
         void OnGet(string path, Func<IHttpRequest, IHttpResponse, Task> listener);
@@ -64,14 +63,14 @@ namespace Butterfly.Core.WebApi {
         /// <summary>
         /// Adds a <paramref name="listener"/> executed when POST requests are received matching <paramref name="path"/>
         /// </summary>
-        /// <remarks>
+        /// <example>
         /// <code>
-        ///     webApi.OnPost("/api/todo", async(req, res) => {
-        ///         var row = await req.ParseAsJsonAsync&lt;Dict&gt;();
-        ///         await database.InsertAndCommitAsync&lt;string&gt;("todo", row);
-        ///     });
+        /// webApi.OnPost("/api/todo", async(req, res) => {
+        ///     var row = await req.ParseAsJsonAsync&lt;Dict&gt;();
+        ///     await database.InsertAndCommitAsync&lt;string&gt;("todo", row);
+        /// });
         /// </code>
-        /// </remarks>
+        /// </example>
         /// <param name="path">Only execute the <paramref name="listener"/> when POST requests match this path.</param>
         /// <param name="listener">Execute this async handler when a POST request is received matching the <paramref name="path"/></param>
         void OnPost(string path, Func<IHttpRequest, IHttpResponse, Task> listener);
@@ -79,14 +78,14 @@ namespace Butterfly.Core.WebApi {
         /// <summary>
         /// Adds a <paramref name="listener"/> executed when PUT requests are received matching <paramref name="path"/>
         /// </summary>
-        /// <remarks>
+        /// <example>
         /// <code>
-        ///     webApi.OnPost("/api/todo", async(req, res) => {
-        ///         var row = await req.ParseAsJsonAsync&lt;Dict&gt;();
-        ///         await database.UpdateAndCommitAsync("todo", row);
-        ///     });
+        /// webApi.OnPost("/api/todo", async(req, res) => {
+        ///     var row = await req.ParseAsJsonAsync&lt;Dict&gt;();
+        ///     await database.UpdateAndCommitAsync("todo", row);
+        /// });
         /// </code>
-        /// </remarks>
+        /// </example>
         /// <param name="path">Only execute the <paramref name="listener"/> when PUT requests match this path.</param>
         /// <param name="listener">Execute this async handler when a PUT request is received matching the <paramref name="path"/></param>
         void OnPut(string path, Func<IHttpRequest, IHttpResponse, Task> listener);
