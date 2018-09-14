92175f43968f419ae0536381b68e00b27c892f65
johan-v-r <jhnvnrnsbrg@gmail.com>
2018-09-14 19:12:24 +0200

    Add butterfly-client

--- Butterfly.Example.Todo/aurelia/src/app.js
+++ Butterfly.Example.Todo/aurelia/src/app.js
@@ -1,5 +1,44 @@
+import { ArrayDataEventHandler, WebSocketChannelClient } from 'butterfly-client';
+
 export class App {
   constructor() {
-    this.message = 'Hello World!';
+    this.message = 'Butterfly Server .NET Aurelia Todo Example'; 
+
+    this.channelClient = null;
+    this.channelClientState = null;
+    this.todoList = [];
+    this.channel = "todos";
+    this.connect();
+    this.subscribe();
+  }
+
+  connect() {
+    let url = `ws://localhost:8000/ws`;
+    this.channelClient = new WebSocketChannelClient({
+      url,
+      onStateChange(value) {
+        this.channelClientState = value;
+      }
+    });
+
+    this.channelClient.connect();
+  }
+
+  subscribe() {
+    this.channelClient.subscribe({
+      channel: this.channel,
+      vars: {
+        clientName: "AureliaWebClient"
+      },
+      handler: new ArrayDataEventHandler({
+        arrayMapping: {
+          todo: this.todoList
+        }
+      })
+    });
+  }
+
+  unsubscribe() {
+    this.channelClient.unsubscribe(this.channel);
   }
 }
