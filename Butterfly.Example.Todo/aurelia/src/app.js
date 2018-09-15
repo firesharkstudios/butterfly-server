09c5d9175ca39d217c0aa04b99cc439f78e9a334
johan-v-r <jhnvnrnsbrg@gmail.com>
2018-09-15 09:53:50 +0200

    Add todo component

--- Butterfly.Example.Todo/aurelia/src/app.js
+++ Butterfly.Example.Todo/aurelia/src/app.js
@@ -1,44 +1,14 @@
-import { ArrayDataEventHandler, WebSocketChannelClient } from 'butterfly-client';
-
 export class App {
   constructor() {
-    this.message = 'Butterfly Server .NET Aurelia Todo Example'; 
-
-    this.channelClient = null;
-    this.channelClientState = null;
-    this.todoList = [];
-    this.channel = "todos";
-    this.connect();
-    this.subscribe();
+    this.message = 'Butterfly Server .NET Aurelia Todo Example';
+    this.todoName = "A new todo item"
   }
 
-  connect() {
-    let url = `ws://localhost:8000/ws`;
-    this.channelClient = new WebSocketChannelClient({
-      url,
-      onStateChange(value) {
-        this.channelClientState = value;
-      }
+  addTodo() {
+    return fetch("http://localhost:8000/api/todo/insert", {
+      method: "POST",
+      body: JSON.stringify({ name: this.todoName }),
+      mode: "no-cors"
     });
-
-    this.channelClient.connect();
-  }
-
-  subscribe() {
-    this.channelClient.subscribe({
-      channel: this.channel,
-      vars: {
-        clientName: "AureliaWebClient"
-      },
-      handler: new ArrayDataEventHandler({
-        arrayMapping: {
-          todo: this.todoList
-        }
-      })
-    });
-  }
-
-  unsubscribe() {
-    this.channelClient.unsubscribe(this.channel);
   }
 }
