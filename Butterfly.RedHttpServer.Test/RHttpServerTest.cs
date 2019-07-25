762df2eb2546cfa19268dc2bcb611bbfd35eca98
rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-07-25 21:51:36 +0200

    Update Red dependency to 4.0.0

--- Butterfly.RedHttpServer.Test/RHttpServerTest.cs
+++ Butterfly.RedHttpServer.Test/RHttpServerTest.cs
@@ -2,34 +2,32 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 using System.Threading.Tasks;
-
+using Butterfly.Core.Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Red;
-
-using Butterfly.Core.Test;
-
-namespace Butterfly.RHttpServer.Test {
+namespace Butterfly.RedHttpServer.Test {
     [TestClass]
     public class RHttpServerTest {
         [TestMethod]
         public async Task ChannelTest() {
-            var webServer = new RedHttpServer(8000);
+            var webServer = new Red.RedHttpServer(8000);
             using (var subscriptionApi = new RedHttpServerSubscriptionApi(webServer, path: "/ws", mustReceiveHeartbeatMillis: 2000, getAuthToken: (authType, authValue) => authValue, getId: authToken => authToken.ToString())) {
                 await ChannelUnitTest.TestChannel(subscriptionApi, "ws://localhost:8000/ws", () => {
-                    webServer.RunAsync();
+                    webServer.Start();
                 });
             }
+            await webServer.StopAsync();
         }
 
         [TestMethod]
         public async Task WebTest() {
-            var webServer = new RedHttpServer(8000);
+            var webServer = new Red.RedHttpServer(8000);
             using (var webApi = new RedHttpServerWebApi(webServer)) {
                 await WebApiUnitTest.TestWeb(webApi, "http://localhost:8000/", () => {
-                    webServer.RunAsync();
+                    webServer.Start();
                 });
             }
+            await webServer.StopAsync();
         }
     }
 }
